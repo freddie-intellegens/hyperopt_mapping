a6d7d05c7b4b74b50521dca65ed937be045c3142
freddie-intellegens <100697870+freddie-intellegens@users.noreply.github.com>
2022-08-05 11:42:16 +0100

    setup

---
+++ main.py
@@ -0,0 +1,19 @@
+hyperparameters = (
+    {
+        "iteration layers": [1, 2, 3],
+        "distribution samples": [5, 10, 20, 50, 100],
+        "data for intercept": 3,
+        "gradient directions": 2,
+        "extra data filters": 0.38483885331937145,
+        "extra data in gradient": 0,
+        "fraction data in intercept": 0.4597690091410343,
+        "data in gradient": 3,
+        "input nodes": 2,
+        "fraction divisions tried": 0.7817864788602976,
+        "steepest permissible gradient": 29.00611324998376,
+        "hybridization tolerance": 0.09566325832957596,
+        "continue training": False,
+        "descriptor columns": 1,
+        "complete columns": 1,
+    },
+)
